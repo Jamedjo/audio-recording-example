a19edf39d562f9ac051d6133b501957d04760476
James Edwards-Jones <git@jamedjo.co.uk>
2021-01-14 17:39:57 +0000

    Simplified example, removing sliders

--- App.tsx
+++ App.tsx
@@ -4,13 +4,13 @@ import {
   Image,
   Slider,
   StyleSheet,
+  StatusBar,
   Text,
   TouchableHighlight,
   View,
 } from "react-native";
 import { Audio, AVPlaybackStatus } from "expo-av";
 import * as FileSystem from "expo-file-system";
-import * as Font from "expo-font";
 import * as Permissions from "expo-permissions";
 import * as Icons from "./components/Icons";
 
@@ -18,7 +18,6 @@ const { width: DEVICE_WIDTH, height: DEVICE_HEIGHT } = Dimensions.get("window");
 const BACKGROUND_COLOR = "#FFF8ED";
 const LIVE_COLOR = "#FF0000";
 const DISABLED_OPACITY = 0.5;
-const RATE_SCALE = 3.0;
 
 type Props = {};
 
@@ -27,16 +26,9 @@ type State = {
   isLoading: boolean;
   isPlaybackAllowed: boolean;
   muted: boolean;
-  soundPosition: number | null;
-  soundDuration: number | null;
-  recordingDuration: number | null;
   shouldPlay: boolean;
   isPlaying: boolean;
   isRecording: boolean;
-  fontLoaded: boolean;
-  shouldCorrectPitch: boolean;
-  volume: number;
-  rate: number;
 };
 
 export default class App extends React.Component<Props, State> {
@@ -57,14 +49,9 @@ export default class App extends React.Component<Props, State> {
       isLoading: false,
       isPlaybackAllowed: false,
       muted: false,
-      soundPosition: null,
-      soundDuration: null,
-      recordingDuration: null,
       shouldPlay: false,
       isPlaying: false,
       isRecording: false,
-      fontLoaded: false,
-      shouldCorrectPitch: true,
       volume: 1.0,
       rate: 1.0,
     };
@@ -81,12 +68,6 @@ export default class App extends React.Component<Props, State> {
   }
 
   componentDidMount() {
-    (async () => {
-      await Font.loadAsync({
-        "cutive-mono-regular": require("./assets/fonts/CutiveMono-Regular.ttf"),
-      });
-      this.setState({ fontLoaded: true });
-    })();
     this._askForPermissions();
   }
 
@@ -100,20 +81,13 @@ export default class App extends React.Component<Props, State> {
   private _updateScreenForSoundStatus = (status: AVPlaybackStatus) => {
     if (status.isLoaded) {
       this.setState({
-        soundDuration: status.durationMillis ?? null,
-        soundPosition: status.positionMillis,
         shouldPlay: status.shouldPlay,
         isPlaying: status.isPlaying,
         rate: status.rate,
-        muted: status.isMuted,
-        volume: status.volume,
-        shouldCorrectPitch: status.shouldCorrectPitch,
         isPlaybackAllowed: true,
       });
     } else {
       this.setState({
-        soundDuration: null,
-        soundPosition: null,
         isPlaybackAllowed: false,
       });
       if (status.error) {
@@ -126,12 +100,10 @@ export default class App extends React.Component<Props, State> {
     if (status.canRecord) {
       this.setState({
         isRecording: status.isRecording,
-        recordingDuration: status.durationMillis,
       });
     } else if (status.isDoneRecording) {
       this.setState({
         isRecording: false,
-        recordingDuration: status.durationMillis,
       });
       if (!this.state.isLoading) {
         this._stopRecordingAndEnablePlayback();
@@ -212,10 +184,6 @@ export default class App extends React.Component<Props, State> {
     const { sound, status } = await this.recording.createNewLoadedSoundAsync(
       {
         isLooping: true,
-        isMuted: this.state.muted,
-        volume: this.state.volume,
-        rate: this.state.rate,
-        shouldCorrectPitch: this.state.shouldCorrectPitch,
       },
       this._updateScreenForSoundStatus
     );
@@ -249,116 +217,13 @@ export default class App extends React.Component<Props, State> {
     }
   };
 
-  private _onMutePressed = () => {
-    if (this.sound != null) {
-      this.sound.setIsMutedAsync(!this.state.muted);
-    }
-  };
-
-  private _onVolumeSliderValueChange = (value: number) => {
-    if (this.sound != null) {
-      this.sound.setVolumeAsync(value);
-    }
-  };
-
-  private _trySetRate = async (rate: number, shouldCorrectPitch: boolean) => {
-    if (this.sound != null) {
-      try {
-        await this.sound.setRateAsync(rate, shouldCorrectPitch);
-      } catch (error) {
-        // Rate changing could not be performed, possibly because the client's Android API is too old.
-      }
-    }
-  };
-
-  private _onRateSliderSlidingComplete = async (value: number) => {
-    this._trySetRate(value * RATE_SCALE, this.state.shouldCorrectPitch);
-  };
-
-  private _onPitchCorrectionPressed = () => {
-    this._trySetRate(this.state.rate, !this.state.shouldCorrectPitch);
-  };
-
-  private _onSeekSliderValueChange = (value: number) => {
-    if (this.sound != null && !this.isSeeking) {
-      this.isSeeking = true;
-      this.shouldPlayAtEndOfSeek = this.state.shouldPlay;
-      this.sound.pauseAsync();
-    }
-  };
-
-  private _onSeekSliderSlidingComplete = async (value: number) => {
-    if (this.sound != null) {
-      this.isSeeking = false;
-      const seekPosition = value * (this.state.soundDuration || 0);
-      if (this.shouldPlayAtEndOfSeek) {
-        this.sound.playFromPositionAsync(seekPosition);
-      } else {
-        this.sound.setPositionAsync(seekPosition);
-      }
-    }
-  };
-
-  private _getSeekSliderPosition() {
-    if (
-      this.sound != null &&
-      this.state.soundPosition != null &&
-      this.state.soundDuration != null
-    ) {
-      return this.state.soundPosition / this.state.soundDuration;
-    }
-    return 0;
-  }
-
-  private _getMMSSFromMillis(millis: number) {
-    const totalSeconds = millis / 1000;
-    const seconds = Math.floor(totalSeconds % 60);
-    const minutes = Math.floor(totalSeconds / 60);
-
-    const padWithZero = (number: number) => {
-      const string = number.toString();
-      if (number < 10) {
-        return "0" + string;
-      }
-      return string;
-    };
-    return padWithZero(minutes) + ":" + padWithZero(seconds);
-  }
-
-  private _getPlaybackTimestamp() {
-    if (
-      this.sound != null &&
-      this.state.soundPosition != null &&
-      this.state.soundDuration != null
-    ) {
-      return `${this._getMMSSFromMillis(
-        this.state.soundPosition
-      )} / ${this._getMMSSFromMillis(this.state.soundDuration)}`;
-    }
-    return "";
-  }
-
-  private _getRecordingTimestamp() {
-    if (this.state.recordingDuration != null) {
-      return `${this._getMMSSFromMillis(this.state.recordingDuration)}`;
-    }
-    return `${this._getMMSSFromMillis(0)}`;
-  }
-
   render() {
-    if (!this.state.fontLoaded) {
-      return <View style={styles.emptyContainer} />;
-    }
-
     if (!this.state.haveRecordingPermissions) {
       return (
         <View style={styles.container}>
           <View />
           <Text
-            style={[
-              styles.noPermissionsText,
-              { fontFamily: "cutive-mono-regular" },
-            ]}
+            style={styles.noPermissionsText}
           >
             You must enable audio recording permissions in order to use this
             app.
@@ -368,301 +233,77 @@ export default class App extends React.Component<Props, State> {
       );
     }
 
+    const playbackOpacity = !this.state.isPlaybackAllowed || this.state.isLoading
+      ? DISABLED_OPACITY
+      : 1.0;
+
     return (
       <View style={styles.container}>
         <View
           style={[
-            styles.halfScreenContainer,
             {
               opacity: this.state.isLoading ? DISABLED_OPACITY : 1.0,
             },
           ]}
         >
-          <View />
-          <View style={styles.recordingContainer}>
-            <View />
-            <TouchableHighlight
-              underlayColor={BACKGROUND_COLOR}
-              style={styles.wrapper}
-              onPress={this._onRecordPressed}
-              disabled={this.state.isLoading}
-            >
-              <Image style={styles.image} source={Icons.RECORD_BUTTON.module} />
-            </TouchableHighlight>
-            <View style={styles.recordingDataContainer}>
-              <View />
-              <Text
-                style={[styles.liveText, { fontFamily: "cutive-mono-regular" }]}
-              >
-                {this.state.isRecording ? "LIVE" : ""}
-              </Text>
-              <View style={styles.recordingDataRowContainer}>
-                <Image
-                  style={[
-                    styles.image,
-                    { opacity: this.state.isRecording ? 1.0 : 0.0 },
-                  ]}
-                  source={Icons.RECORDING.module}
-                />
-                <Text
-                  style={[
-                    styles.recordingTimestamp,
-                    { fontFamily: "cutive-mono-regular" },
-                  ]}
-                >
-                  {this._getRecordingTimestamp()}
-                </Text>
-              </View>
-              <View />
-            </View>
-            <View />
-          </View>
-          <View />
-        </View>
-        <View
-          style={[
-            styles.halfScreenContainer,
-            {
-              opacity:
-                !this.state.isPlaybackAllowed || this.state.isLoading
-                  ? DISABLED_OPACITY
-                  : 1.0,
-            },
-          ]}
-        >
-          <View />
-          <View style={styles.playbackContainer}>
-            <Slider
-              style={styles.playbackSlider}
-              trackImage={Icons.TRACK_1.module}
-              thumbImage={Icons.THUMB_1.module}
-              value={this._getSeekSliderPosition()}
-              onValueChange={this._onSeekSliderValueChange}
-              onSlidingComplete={this._onSeekSliderSlidingComplete}
-              disabled={!this.state.isPlaybackAllowed || this.state.isLoading}
-            />
-            <Text
-              style={[
-                styles.playbackTimestamp,
-                { fontFamily: "cutive-mono-regular" },
-              ]}
-            >
-              {this._getPlaybackTimestamp()}
-            </Text>
-          </View>
-          <View
-            style={[styles.buttonsContainerBase, styles.buttonsContainerTopRow]}
+          <StatusBar backgroundColor={BACKGROUND_COLOR} barStyle="dark-content" />
+          <TouchableHighlight
+            underlayColor={BACKGROUND_COLOR}
+            style={styles.wrapper}
+            onPress={this._onRecordPressed}
+            disabled={this.state.isLoading}
           >
-            <View style={styles.volumeContainer}>
-              <TouchableHighlight
-                underlayColor={BACKGROUND_COLOR}
-                style={styles.wrapper}
-                onPress={this._onMutePressed}
-                disabled={!this.state.isPlaybackAllowed || this.state.isLoading}
-              >
-                <Image
-                  style={styles.image}
-                  source={
-                    this.state.muted
-                      ? Icons.MUTED_BUTTON.module
-                      : Icons.UNMUTED_BUTTON.module
-                  }
-                />
-              </TouchableHighlight>
-              <Slider
-                style={styles.volumeSlider}
-                trackImage={Icons.TRACK_1.module}
-                thumbImage={Icons.THUMB_2.module}
-                value={1}
-                onValueChange={this._onVolumeSliderValueChange}
-                disabled={!this.state.isPlaybackAllowed || this.state.isLoading}
-              />
-            </View>
-            <View style={styles.playStopContainer}>
-              <TouchableHighlight
-                underlayColor={BACKGROUND_COLOR}
-                style={styles.wrapper}
-                onPress={this._onPlayPausePressed}
-                disabled={!this.state.isPlaybackAllowed || this.state.isLoading}
-              >
-                <Image
-                  style={styles.image}
-                  source={
-                    this.state.isPlaying
-                      ? Icons.PAUSE_BUTTON.module
-                      : Icons.PLAY_BUTTON.module
-                  }
-                />
-              </TouchableHighlight>
-              <TouchableHighlight
-                underlayColor={BACKGROUND_COLOR}
-                style={styles.wrapper}
-                onPress={this._onStopPressed}
-                disabled={!this.state.isPlaybackAllowed || this.state.isLoading}
-              >
-                <Image style={styles.image} source={Icons.STOP_BUTTON.module} />
-              </TouchableHighlight>
-            </View>
-            <View />
-          </View>
-          <View
+            <Image style={styles.image} source={Icons.RECORD_BUTTON.module} />
+          </TouchableHighlight>
+          <Image
             style={[
-              styles.buttonsContainerBase,
-              styles.buttonsContainerBottomRow,
+              styles.image,
+              { opacity: this.state.isRecording ? 1.0 : 0.0 },
             ]}
-          >
-            <Text style={styles.timestamp}>Rate:</Text>
-            <Slider
-              style={styles.rateSlider}
-              trackImage={Icons.TRACK_1.module}
-              thumbImage={Icons.THUMB_1.module}
-              value={this.state.rate / RATE_SCALE}
-              onSlidingComplete={this._onRateSliderSlidingComplete}
-              disabled={!this.state.isPlaybackAllowed || this.state.isLoading}
-            />
-            <TouchableHighlight
-              underlayColor={BACKGROUND_COLOR}
-              style={styles.wrapper}
-              onPress={this._onPitchCorrectionPressed}
-              disabled={!this.state.isPlaybackAllowed || this.state.isLoading}
-            >
-              <Text style={[{ fontFamily: "cutive-mono-regular" }]}>
-                PC: {this.state.shouldCorrectPitch ? "yes" : "no"}
-              </Text>
-            </TouchableHighlight>
-          </View>
-          <View />
+            source={Icons.RECORDING.module}
+          />
         </View>
+        <TouchableHighlight
+          underlayColor={BACKGROUND_COLOR}
+          style={[ { opacity: playbackOpacity }, styles.wrapper]}
+          onPress={this._onPlayPausePressed}
+          disabled={!this.state.isPlaybackAllowed || this.state.isLoading}
+        >
+          <Image
+            style={styles.image}
+            source={
+              this.state.isPlaying
+                ? Icons.PAUSE_BUTTON.module
+                : Icons.PLAY_BUTTON.module
+            }
+          />
+        </TouchableHighlight>
+        <TouchableHighlight
+          underlayColor={BACKGROUND_COLOR}
+          style={[ { opacity: playbackOpacity }, styles.wrapper]}
+          onPress={this._onStopPressed}
+          disabled={!this.state.isPlaybackAllowed || this.state.isLoading}
+        >
+          <Image style={styles.image} source={Icons.STOP_BUTTON.module} />
+        </TouchableHighlight>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  emptyContainer: {
-    alignSelf: "stretch",
-    backgroundColor: BACKGROUND_COLOR,
-  },
   container: {
     flex: 1,
-    flexDirection: "column",
-    justifyContent: "space-between",
+    flexDirection: "row",
+    justifyContent: "space-around",
     alignItems: "center",
-    alignSelf: "stretch",
     backgroundColor: BACKGROUND_COLOR,
-    minHeight: DEVICE_HEIGHT,
-    maxHeight: DEVICE_HEIGHT,
   },
   noPermissionsText: {
     textAlign: "center",
   },
   wrapper: {},
-  halfScreenContainer: {
-    flex: 1,
-    flexDirection: "column",
-    justifyContent: "space-between",
-    alignItems: "center",
-    alignSelf: "stretch",
-    minHeight: DEVICE_HEIGHT / 2.0,
-    maxHeight: DEVICE_HEIGHT / 2.0,
-  },
-  recordingContainer: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    alignSelf: "stretch",
-    minHeight: Icons.RECORD_BUTTON.height,
-    maxHeight: Icons.RECORD_BUTTON.height,
-  },
-  recordingDataContainer: {
-    flex: 1,
-    flexDirection: "column",
-    justifyContent: "space-between",
-    alignItems: "center",
-    minHeight: Icons.RECORD_BUTTON.height,
-    maxHeight: Icons.RECORD_BUTTON.height,
-    minWidth: Icons.RECORD_BUTTON.width * 3.0,
-    maxWidth: Icons.RECORD_BUTTON.width * 3.0,
-  },
-  recordingDataRowContainer: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    minHeight: Icons.RECORDING.height,
-    maxHeight: Icons.RECORDING.height,
-  },
-  playbackContainer: {
-    flex: 1,
-    flexDirection: "column",
-    justifyContent: "space-between",
-    alignItems: "center",
-    alignSelf: "stretch",
-    minHeight: Icons.THUMB_1.height * 2.0,
-    maxHeight: Icons.THUMB_1.height * 2.0,
-  },
-  playbackSlider: {
-    alignSelf: "stretch",
-  },
-  liveText: {
-    color: LIVE_COLOR,
-  },
-  recordingTimestamp: {
-    paddingLeft: 20,
-  },
-  playbackTimestamp: {
-    textAlign: "right",
-    alignSelf: "stretch",
-    paddingRight: 20,
-  },
   image: {
     backgroundColor: BACKGROUND_COLOR,
-  },
-  textButton: {
-    backgroundColor: BACKGROUND_COLOR,
-    padding: 10,
-  },
-  buttonsContainerBase: {
-    flex: 1,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-  },
-  buttonsContainerTopRow: {
-    maxHeight: Icons.MUTED_BUTTON.height,
-    alignSelf: "stretch",
-    paddingRight: 20,
-  },
-  playStopContainer: {
-    flex: 1,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    minWidth: ((Icons.PLAY_BUTTON.width + Icons.STOP_BUTTON.width) * 3.0) / 2.0,
-    maxWidth: ((Icons.PLAY_BUTTON.width + Icons.STOP_BUTTON.width) * 3.0) / 2.0,
-  },
-  volumeContainer: {
-    flex: 1,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    minWidth: DEVICE_WIDTH / 2.0,
-    maxWidth: DEVICE_WIDTH / 2.0,
-  },
-  volumeSlider: {
-    width: DEVICE_WIDTH / 2.0 - Icons.MUTED_BUTTON.width,
-  },
-  buttonsContainerBottomRow: {
-    maxHeight: Icons.THUMB_1.height,
-    alignSelf: "stretch",
-    paddingRight: 20,
-    paddingLeft: 20,
-  },
-  timestamp: {
-    fontFamily: "cutive-mono-regular",
-  },
-  rateSlider: {
-    width: DEVICE_WIDTH / 2.0,
   },
 });
